c773ecf052415f35ec5cca0eebca66a6a1589a61
Irina14 <ira1786@bk.ru>
2019-10-30 16:43:49 +0700

    Добавляет шапку

---
+++ source/js/scripts/menu.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  var menu = document.querySelector('.header__menu');
+
+  if (menu) {
+    var ESC_KEYCODE = 27;
+    var menuButton = document.querySelector('.header__toggle');
+
+    var documentClickHandler = function (evt) {
+      if (evt.target !== menuButton) {
+        menu.classList.remove('header__menu--open');
+        menuButton.classList.remove('header__toggle--open');
+      }
+    };
+
+    var documentEscKeyHandler = function (evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        menu.classList.remove('header__menu--open');
+        menuButton.classList.remove('header__toggle--open');
+      }
+    };
+
+    menuButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      menu.classList.toggle('header__menu--open');
+      menuButton.classList.toggle('header__toggle--open');
+
+      if (menu.classList.contains('header__menu--open')) {
+        document.addEventListener('click', documentClickHandler);
+        document.addEventListener('keydown', documentEscKeyHandler);
+      } else {
+        document.removeEventListener('click', documentClickHandler);
+        document.removeEventListener('keydown', documentEscKeyHandler);
+      }
+    });
+  }
+})();
