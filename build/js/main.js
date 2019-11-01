03ac0eacaadea390e1d86f7caecc9f3260dce1c3
Irina14 <ira1786@bk.ru>
2019-11-01 20:30:26 +0700

    Добавляет слайдер на планшетную версию

--- build/js/main.js
+++ build/js/main.js
@@ -37,4 +37,15 @@
   }
 })();
 
+'use strict';
+
+var mySwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 3,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 //# sourceMappingURL=main.js.map
---
+++ source/js/scripts/slider.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mySwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 3,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
