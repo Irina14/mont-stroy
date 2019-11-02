f15169b5ad255ad63fb24a5bcef4a733d5c9df95
Irina14 <ira1786@bk.ru>
2019-11-02 15:18:07 +0700

    Добавляет слайдер преимущества

--- source/js/scripts/slider.js
+++ source/js/scripts/slider.js
@@ -1,10 +1,39 @@
 'use strict';
 
-var mySwiper = new Swiper ('.swiper-container', {
-  slidesPerView: 3,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+var swiperContainer = document.querySelector('.swiper-container');
+
+if (swiperContainer) {
+  var swiper = false;
+
+  var initSwiper = function () {
+    var widthWindow = window.innerWidth;
+
+    if (widthWindow < 1024 && swiper === false) {
+      swiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+        }
+      });
+    } else if (widthWindow > 1024 && swiper !== false) {
+      swiper.destroy();
+      swiper = false;
+    }
+  };
+
+  initSwiper();
+
+  window.addEventListener('resize', function () {
+    initSwiper();
+  });
+}
+
