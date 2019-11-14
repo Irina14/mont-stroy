6e55864601c9dea1ad61504c55fb11bf03adcfdd
Irina14 <ira1786@bk.ru>
2019-11-15 01:05:15 +0700

    Добавляет правки

--- source/js/scripts/transition.js
+++ source/js/scripts/transition.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var TIME = 600;
+
   // Плавный переход по якорной ссылке
   var setSmoothTransition = function (selector, time) {
     $(document).ready(function () {
@@ -16,32 +18,32 @@
   var header = document.querySelector('.header');
 
   if (header) {
-    setSmoothTransition('.header__button', 2000);
-    setSmoothTransition('.header__item a', 1500);
+    setSmoothTransition('.header__button', TIME);
+    setSmoothTransition('.header__item a', TIME);
   }
 
   var mainScreen = document.querySelector('.main-screen');
 
   if (mainScreen) {
-    setSmoothTransition('.main-screen__button--services', 1500);
-    setSmoothTransition('.main-screen__button--contacts', 2000);
+    setSmoothTransition('.main-screen__button--services', TIME);
+    setSmoothTransition('.main-screen__button--contacts', TIME);
   }
 
   var footerMenuDown = document.querySelector('.footer__menu-down');
 
   if (footerMenuDown) {
-    setSmoothTransition('.footer__menu-down .footer__menu-item a', 1500);
+    setSmoothTransition('.footer__menu-down .footer__menu-item a', TIME);
   }
 
   var footerMenuUp = document.querySelector('.footer__menu-up');
 
   if (footerMenuUp) {
-    setSmoothTransition('.footer__menu-up .footer__menu-item a', 1500);
+    setSmoothTransition('.footer__menu-up .footer__menu-item a', TIME);
   }
 
   var footerLogo = document.querySelector('.footer__logo');
 
   if (footerLogo) {
-    setSmoothTransition('.footer__logo', 2000);
+    setSmoothTransition('.footer__logo', TIME);
   }
 })();
