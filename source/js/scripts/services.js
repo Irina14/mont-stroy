1a7d8e1c8c7f1818a972afc061365c2b76a6e770
Irina14 <ira1786@bk.ru>
2019-11-15 14:54:45 +0700

    Добавляет правки

--- source/js/scripts/services.js
+++ source/js/scripts/services.js
@@ -5,55 +5,22 @@
 
   if (services) {
     var servicesItems = services.querySelectorAll('.services__item');
-    var servicesBlockActive = services.querySelector('.services__block-active');
 
-    var removeClass = function () {
+    var closeBlocks = function () {
       Array.prototype.slice.call(servicesItems).forEach(function (item) {
         item.classList.remove('services__item--active');
       });
     };
 
-    var itemClickHandlerTablet = function (item) {
-      removeClass();
-      var itemCopy = item.cloneNode(true);
-      item.classList.add('services__item--active');
-      servicesBlockActive.innerHTML = '';
-      servicesBlockActive.appendChild(itemCopy);
-    };
-
-    var itemClickHandlerMobile = function (item) {
-      if (!item.classList.contains('services__item--active')) {
-        removeClass();
-        item.classList.add('services__item--active');
-      }
-    };
-
-    var initAccordion = function () {
-      var widthWindow = window.innerWidth;
-
-      if (widthWindow < 1024 && widthWindow >= 768) {
-        Array.prototype.slice.call(servicesItems).forEach(function (item) {
-          item.removeEventListener('click', itemClickHandlerMobile);
-          item.addEventListener('click', function () {
-            itemClickHandlerTablet(item);
-          });
-        });
-      }
-
-      if (widthWindow < 768) {
-        Array.prototype.slice.call(servicesItems).forEach(function (item) {
-          item.removeEventListener('click', itemClickHandlerTablet);
-          item.addEventListener('click', function () {
-            itemClickHandlerMobile(item);
-          });
-        });
-      }
-    };
-
-    initAccordion();
-
-    window.addEventListener('resize', function () {
-      initAccordion();
+    Array.prototype.slice.call(servicesItems).forEach(function (item) {
+      item.addEventListener('click', function () {
+        if (item.classList.contains('services__item--active')) {
+          item.classList.remove('services__item--active');
+        } else {
+          closeBlocks();
+          item.classList.add('services__item--active');
+        }
+      });
     });
   }
 })();
