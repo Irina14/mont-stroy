a99a3a016adf81dd1febaa6a57cf5475679a41ff
Irina14 <ira1786@bk.ru>
2019-11-15 17:19:35 +0700

    Добавляет правки

--- source/js/scripts/transition.js
+++ source/js/scripts/transition.js
@@ -3,47 +3,43 @@
 (function () {
   var TRANSITION_TIME = 600;
 
-  // Плавный переход по якорной ссылке
-  var setSmoothTransition = function (selector, time) {
-    $(document).ready(function () {
-      $(selector).on('click', function (evt) {
-        evt.preventDefault();
-        var id = $(this).attr('href');
-        var top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, time);
-      });
-    });
-  };
-
   var header = document.querySelector('.header');
 
   if (header) {
-    setSmoothTransition('.header__button', TRANSITION_TIME);
-    setSmoothTransition('.header__item a', TRANSITION_TIME);
+    var scroll = new SmoothScroll('.header__button, .header__item a', {
+      speed: TRANSITION_TIME
+    });
   }
 
   var mainScreen = document.querySelector('.main-screen');
 
   if (mainScreen) {
-    setSmoothTransition('.main-screen__button--services', TRANSITION_TIME);
-    setSmoothTransition('.main-screen__button--contacts', TRANSITION_TIME);
+    var scroll = new SmoothScroll('.main-screen__button--services, .main-screen__button--contacts', {
+      speed: TRANSITION_TIME
+    });
   }
 
   var footerMenuDown = document.querySelector('.footer__menu-down');
 
   if (footerMenuDown) {
-    setSmoothTransition('.footer__menu-down .footer__menu-item a', TRANSITION_TIME);
+    var scroll = new SmoothScroll('.footer__menu-down .footer__menu-item a', {
+      speed: TRANSITION_TIME
+    });
   }
 
   var footerMenuUp = document.querySelector('.footer__menu-up');
 
   if (footerMenuUp) {
-    setSmoothTransition('.footer__menu-up .footer__menu-item a', TRANSITION_TIME);
+    var scroll = new SmoothScroll('.footer__menu-up .footer__menu-item a', {
+      speed: TRANSITION_TIME
+    });
   }
 
   var footerLogo = document.querySelector('.footer__logo');
 
   if (footerLogo) {
-    setSmoothTransition('.footer__logo', TRANSITION_TIME);
+    var scroll = new SmoothScroll('.footer__logo', {
+      speed: TRANSITION_TIME
+    });
   }
 })();
